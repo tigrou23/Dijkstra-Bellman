1a49a7df36d64e6567d88fa02ec51f0670aa5ec4
tigrou23 <hugo0807pereira@gmail.com>
2022-04-24 08:10:17 +0200

    amélioration Dijkstra

--- src/graphe/PCCDijkstra.java
+++ src/graphe/PCCDijkstra.java
@@ -39,9 +39,7 @@ public class PCCDijkstra{
 		HashMap<Integer, Integer> tabPredecesseur = new HashMap<>(); //clé : un sommet --- valeur : le prédecesseur pour y arriver			
 		for (int i = 0; i < graphe.getNbNoeuds(); i++) //on initialise tous les couts pour arriver aux arcs à -1 (je simule l+infini du tableau)
 			tabPoids.put(i, -1);
-			
-		tabPoids.put(debut - 1, 0);//on met le sommet de départ à 0 (comme dans le tableau)
-			
+		tabPoids.put(debut - 1, 0);//on met le sommet de départ à 0 (comme dans le tableau)	
 		debut -= 1;//ça simplifie pour utiliser comme index dans les tableaux
 		while (debut + 1 != fin) {
 			tabPoids.put(debut, 0); //on met le sommet qu'on vient d'entourer à 0 pour pas qu'il soit pris dans nos calculs. Je ne pouvais pas le supprimer de la Hashmap car ça faussait les boucles (qui se basent sur le nombre de sommets dans la Hashmap)
@@ -49,23 +47,15 @@ public class PCCDijkstra{
 			for (int i = 0; i < graphe.getNbNoeuds(); i++) { 
 				if (listeArc.contains(graphe.getArc(debut + 1, i + 1))) { // on regarde si l'arc qu'on crée avec la boucle est un arc qui part du sommet (c-a-d qu'il se trouve dans l'ArrayList des arcs du sommet)
 					somme = poids + graphe.getArc(debut + 1, i + 1).getPoids(); //cf. ligne 58
-					if (tabPoids.get(i) == -1) { // si il y a un + infini dans la case (-1) et que ce n'est pas égal à 0 (ça voudrait dire que le sommet est barré) alors on met comme valeur le poids (cf. ligne 68)
+					if (tabPoids.get(i) == -1 || somme < tabPoids.get(i)) { // si il y a un + infini dans la case (-1) et que ce n'est pas égal à 0 (ça voudrait dire que le sommet est barré) alors on met comme valeur le poids (cf. ligne 68)
 						tabPoids.put(graphe.getArc(debut + 1, i + 1).getSommet2() - 1,
 								poids + graphe.getArc(debut + 1, i + 1).getPoids()); // on ajoute donc le poids + le poids de l'arc.
 						tabPredecesseur.put(graphe.getArc(debut + 1, i + 1).getSommet2() - 1, debut); // on marque le sommet précédent pour retrouver le chemin après
 					}
-					
-					else if (somme < tabPoids.get(i)) { // si il y a déjà une valeur dans la case. On vérifié que si on additionne le poids avec le poid de l'arc c'est plus petit que la valeur de la case. Si c'est plus petit on remplace la valeur et on remplace le sommet précédent par le nouveau
-						tabPoids.put(graphe.getArc(debut + 1, i + 1).getSommet2() - 1,
-								poids + graphe.getArc(debut + 1, i + 1).getPoids());
-						tabPredecesseur.put(graphe.getArc(debut + 1, i + 1).getSommet2() - 1, debut);
-					}
 				}
 			}
-			
 			tmp.clear();
 			tmp.addAll(tabPoids.values());
-			
 			for (int i = 0; i < tabPoids.size(); i++) { //ici on supprime tous les -1 et 0. Alors je savais pas mais pour supprimer dans une liste c'est juste un enfer (à cause des index). Donc faudra voir comme on peut améliorer ça mais sur internet j'ai rien trouvé
 				tmp.remove(new Integer(0));
 				tmp.remove(new Integer(-1));
@@ -88,13 +78,7 @@ public class PCCDijkstra{
 			chemin.add(temporaire + 1);
 		}
 		Collections.reverse(chemin); //explicite
-		
-		//juste pour afficher (pour mes tests perso)
-		/*for (int i = 0; i < chemin.size(); i++) {
-			System.out.print(chemin.get(i) + " ");
-		}*/
 		return chemin;
-		
 	}
 	
 }
