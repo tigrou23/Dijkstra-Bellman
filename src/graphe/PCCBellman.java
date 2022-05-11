c1082b1ee3c26b048413e19a19df3b3c32dcc779
Félix <felix.liburski@gmail.com>
2022-05-11 21:08:18 +0200

    poids

--- src/graphe/PCCBellman.java
+++ src/graphe/PCCBellman.java
@@ -188,6 +188,8 @@ public class PCCBellman implements AlgoPlusCourt{
 				if(i.getKey()==sommet) {
 					chemin.add(sommet);
 					sommet=i.getValue();
+					if(chemin.get(chemin.size()-2) != null)
+						this.poids += graphe.getArc(chemin.get(chemin.size()-1), chemin.get(chemin.size()-2)).getPoids();
 					break;
 				}
 			}
